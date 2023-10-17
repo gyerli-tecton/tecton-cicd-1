a6f578f9460c69b149791da641b60560d4ed9765
gyerli-tecton <gursoy@tecton.ai>
2023-10-16 20:15:47 -0500

    modified workflow files and created data source objects on repo.py

--- repo.py
+++ repo.py
@@ -1,14 +1,16 @@
 from tecton import HiveConfig, BatchSource
 from datetime import datetime, timedelta
 
-# A HiveConfig object that points to the table containing the raw data
+# A HiveConfig object that points to the 
+# table containing the raw data
 hive_config = HiveConfig(
     database="gursoy_fraud_simple",
     table="txn",
     timestamp_field="timestamp"
 )
 
-# A BatchSource object that points to the Hive table and specifies the owner
+# A BatchSource object that points to 
+# the Hive table and specifies the owner
 txn_batch = BatchSource(
     name="txn_batch",
     batch_config=hive_config,
