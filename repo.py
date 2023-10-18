39321264c892929a9f7852aa76afd5062ef55c15
gyerli-tecton <gursoy@tecton.ai>
2023-10-18 13:11:55 -0500

    added FV to repo.py

--- repo.py
+++ repo.py
@@ -1,6 +1,12 @@
 from tecton import HiveConfig, BatchSource
 from datetime import datetime, timedelta
 from tecton import Entity
+from tecton import (
+    FeatureView,
+    FilteredSource,
+    Aggregation,
+    materialization_context,
+    batch_feature_view)
 
 # A HiveConfig object that points to the table containing the raw data
 hive_config = HiveConfig(
@@ -20,3 +26,27 @@ user = Entity(
     name='user',
     join_keys=['user_id']
 )
+
+# A FeatureView that ingests data from the BatchSource defined above
+@batch_feature_view(
+    name="user_txns_custom_7d",``
+    sources=[FilteredSource(txn_batch, start_time_offset=timedelta(days=-6))],
+    entities=[user],
+    mode="spark_sql",
+    online=True,
+    offline=True,
+    batch_schedule=timedelta(days=1),
+    ttl=timedelta(days=1),
+    feature_start_time=datetime(2023, 8, 10),
+    incremental_backfills=True
+)
+def user_txns_custom_7d(txn_batch, context=materialization_context()):
+    return f"""
+        select
+            user_id,
+            to_timestamp("{context.end_time}") - interval 1 microsecond as timestamp,
+            count(user_id) as cnt
+        from
+            {txn_batch}
+        group by user_id
+        """
